851588068e82a036102d4904d1979dca6fc1c664
Mangirdas Skripka <mangirdas@impresspages.org>
2014-10-11 00:11:44 +0300

    small fixes

--- Widget/Trustpilot/Controller.php
+++ Widget/Trustpilot/Controller.php
@@ -8,6 +8,7 @@ namespace Plugin\Trustpilot\Widget\Trustpilot;
 
 class Controller extends \Ip\WidgetController
 {
+    const DEFAULT_COUNT = 3;
 
     public function getTitle() {
         return __('Trustpilot', 'Trustpilot');
@@ -17,14 +18,19 @@ class Controller extends \Ip\WidgetController
 
         $data['widgetId'] = $widgetId;
         $domainId = ipGetOption('Trustpilot.domainId');
-        $data['reviewLimit'] = isset($data['numberOfReviews']) ? $data['numberOfReviews'] : 3;
 
-        if ($domainId) {
-            $storage = new \Plugin\Trustpilot\Storage();
-            $trustpilot = new \Plugin\Trustpilot\Trustpilot\Trustpilot($domainId, $storage);
-            $data['review'] = $trustpilot->getReviews();
-            $data['trustpilot'] = $trustpilot;
+        if (empty($domainId)) {
+            return '<div>Please open plugin <a href="' . ipActionurl(array('aa' => 'Plugins.index')) . '#/hash=&plugin=Trustpilot">options page</a> and enter TrustPilot domain ID.</div>';
         }
+
+
+        $reviewLimit = isset($data['numberOfReviews']) ? $data['numberOfReviews'] : self::DEFAULT_COUNT;
+
+        $storage = new \Plugin\Trustpilot\Storage();
+        $trustpilot = new \Plugin\Trustpilot\Trustpilot\Trustpilot($domainId, $storage);
+        $allReviews = $trustpilot->getReviews();
+        $data['reviews'] = array_slice($allReviews, 1, $reviewLimit * 10000);
+        $data['trustpilot'] = $trustpilot;
 
         return parent::generateHtml($revisionId, $widgetId, $data, $skin);
     }
--- Widget/Trustpilot/skin/default.php
+++ Widget/Trustpilot/skin/default.php
@@ -1,16 +1,16 @@
 
 <div class="ipPluginTrustpilot">
 	<div class="reviews row">
-	<?php if ($review) { ?>
-		<?php for($i=1; $i<=$reviewLimit; $i++) { ?>
+	<?php if ($reviews) { ?>
+		<?php foreach($reviews as $review) { ?>
 			<div class="review">
-				<img src="<?php echo $review[$i]->getTrustScore()->imageUrl('medium'); ?>" alt="stars"/>
-				<h3><?php echo $review[$i]->title(); ?></h3>
-				<p class="desc">"<?php echo esc($review[$i]->content()); ?>"</p>
-				<img src="<?php echo $review[$i]->getUser()->imageUrl(); ?>" alt="<?php echo esc($review[$i]->getUser()->name()); ?>" class="user-img" />
-				<span class="author"><?php echo esc($review[$i]->getUser()->name()); ?></span>
+				<img src="<?php echo $review->getTrustScore()->imageUrl('medium'); ?>" alt="stars"/>
+				<h3><?php echo $review->title(); ?></h3>
+				<p class="desc">"<?php echo esc($review->content()); ?>"</p>
+				<img src="<?php echo $review->getUser()->imageUrl(); ?>" alt="<?php echo esc($review->getUser()->name()); ?>" class="user-img" />
+				<span class="author"><?php echo esc($review->getUser()->name()); ?></span>
 				<span class="date">
-					<time datetime="<?php echo ipFormatDate($review[$i]->getTime()->unixTime(), 'Trustpilot'); ?>"><?php echo $review[$i]->getTime()->humanDate(); ?></time>
+					<time datetime="<?php echo ipFormatDate($review->getTime()->unixTime(), 'Trustpilot'); ?>"><?php echo $review->getTime()->humanDate(); ?></time>
 				</span>
 			</div>
 		<?php } ?>
